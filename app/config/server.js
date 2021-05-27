fab240abf05ced79f52653bde11bfa54aa05e139
Kacper Lukasik <ukasikpl@outlook.com>
2021-05-27 14:36:10 +0200

    add strapi to project, changle database to postgresql

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '110a5871f02b3873b06f11822ef178eb'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0f9b9ce7-3797-409a-9a1a-1bfa012a12a8'
+};
